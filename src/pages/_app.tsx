171f5520cb3987b53e82c92b8834fd5195117b91
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-04-08 15:39:41 -0300

    feat: global styles and themes

---
+++ src/pages/_app.tsx
@@ -0,0 +1,90 @@
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AppProps } from 'next/app'
+import { FC, useState } from 'react'
+import Image from 'next/image'
+
+export const lightTheme = {
+  colors: {
+    primary: '#018762',
+    secondary: '#FFFFFF',
+    text1: '#1F1F1F',
+    text2: '#515151',
+  },
+}
+
+export const darkTheme = {
+  colors: {
+    primary: '#018762',
+    secondary: '#1F1F1F',
+    text1: '#FFFFFF',
+    text2: '#919191',
+  },
+}
+
+const GlobalStyle = createGlobalStyle<{ theme: typeof lightTheme }>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Nunito', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.secondary};
+  }
+
+  span img {
+    pointer-events: none;
+  }
+
+  ::selection {
+    color: #EEEEEE;
+    background-color: #018762;
+  }
+`
+
+const Toggle = styled.span`
+  position: absolute;
+  left: 45%;
+  top: 1rem;
+  cursor: pointer;
+
+  @media (max-width: 900px) {
+    left: 27%;
+  }
+
+  @media (max-width: 520px) {
+    left: 5%;
+    width: 20px;
+  }
+`
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const [isDarkMode, setDarkMode] = useState(false)
+
+  const handleDarkMode = () => setDarkMode(!isDarkMode)
+
+  return (
+    <>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <Toggle onClick={handleDarkMode}>
+          <Image
+            src={isDarkMode ? '/icons/on.svg' : '/icons/off.svg'}
+            width={25}
+            height={25}
+            alt={isDarkMode ? 'Modo Claro' : 'Modo Escuro'}
+          />
+        </Toggle>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
